ea0375d30cf9c8a7c7178bc3b3c823cb32340add
Pd1589 <84357548+Pd1589@users.noreply.github.com>
2021-05-25 20:42:40 -0400

    Validated User Choice

--- game.py
+++ game.py
@@ -3,3 +3,13 @@
 print("Rock, Paper, Scissors, Shoot!")
 user_choice = input("Please choose one of: 'Rock' ,'Paper' ,'Scissors' --->")
 print("USER CHOICE: ", user_choice)
+#EXIT IF INVALID ENTRY 
+if (user_choice == "Rock") or (user_choice == "Paper")or (user_choice == "Scissors"):
+    print("VALID ENTRY KEEP GOING")
+else:
+    print("Invalid Entry, Please Try Program Again")
+    exit()
+    
+
+
+print("END OF GAME, THANKS FOR PLAYING")
